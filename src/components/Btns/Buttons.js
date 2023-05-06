c12c9913a65c93c8aff3fe453cb7c1d05a3227ba
iKalashnikova <ikalashnikova@ukr.net>
2023-05-06 23:06:25 +0200

    .

--- src/components/Btns/Buttons.js
+++ src/components/Btns/Buttons.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Buttons} from './Btns.styled'
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Feedback, Span } from './Statistics.styled';
 
 export const Statistics = ({total, percentage, good, neutral, bad }) => (
